52d00762bbcecda81bb534142d99d4e0f09eee04
Brody001 <byunghak@fastlane.kr>
2025-07-19 10:03:12 +0900

    Initial Commit

---
+++ teto-egen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teto-egen
+//
+//  Created by 고병학 on 7/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
